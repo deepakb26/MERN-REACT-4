002e5814bc0ef79da3899212e6ba37355d59da4f
Deepak <53331181+deepakb26@users.noreply.github.com>
2023-10-14 23:46:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import UserData from './UserData'; 
+
+function App() {
+  return (
+    <div>
+    <div className="App">
+      <UserData />
+    </div>
+   
+    </div>
+  );
+}
+
+export default App;
